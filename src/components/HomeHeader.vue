7ae60ec80ec3adfffb6ae2480bde54111a694923
黄志平 <10049642+hzp13960020433@user.noreply.gitee.com>
2022-06-28 18:39:02 +0800

    init

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <div class="header-left"></div>
+    <div class="header-input"></div>
+    <div class="header-right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'homeHeader',
+  data () {
+    return {
+      msg: 'homeHeader'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="" scoped>
+.header {
+  height: 0.86rem;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="home">
+    <HomeHeader></HomeHeader>
+    hello world
+  </div>
+</template>
+
+<script>
+import HomeHeader from '../components/HomeHeader.vue'
+export default {
+
+  name: 'Home',
+  components: { HomeHeader: HomeHeader }
+}
+</script>
