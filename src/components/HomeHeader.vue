5c0dc3e47ff2734f3ce6b48fea89014194f19716
黄志平 <10049642+hzp13960020433@user.noreply.gitee.com>
2022-06-29 09:14:57 +0800

    add homeHeader

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="header">
-    <div class="header-left"></div>
-    <div class="header-input"></div>
-    <div class="header-right"></div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe685;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe651;</span>
+    </div>
+    <div class="header-right">城市
+      <span class="iconfont arrow-icon">&#xe688;</span>
+    </div>
   </div>
 </template>
 
@@ -18,8 +24,41 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="" scoped>
+<style  scoped>
+/* @import '~@/assets/styles/varibles.styl'; */
 .header {
-  height: 0.86rem;
+  background-color: #00bcd4;
+  display: flex;
+  line-height: 0.86rem;
+  color: #fff;
+  /* height: 43px; */
+}
+.header .header-left {
+  width: 0.64rem;
+  float: left;
+}
+.header .header-left .back-icon {
+  text-align: center;
+  font-size: 0.4rem;
+}
+.header .header-input {
+  height: 0.64rem;
+  line-height: 0.64rem;
+  background-color: #fff;
+  border-radius: 0.1rem;
+  margin-left: 0.2rem;
+  margin-top: 0.12rem;
+  padding-left: 0.2rem;
+  flex: 1;
+  color: #ccc;
+}
+.header .header-right {
+  width: 1.24rem;
+  float: right;
+  text-align: center;
+}
+.header .header-right .arrow-icon {
+  margin-left: -0.04rem;
+  font-size: 0.24rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import './assets/styles/border.css'
 import './assets/styles/reset.css'
-
+import './assets/styles/iconfont/iconfont.css'
 Vue.config.productionTip = false
 // fastClick.attach(document.body)
 /* eslint-disable no-new */
